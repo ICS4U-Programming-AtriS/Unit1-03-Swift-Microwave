7ebea416553a3c0d1e3d06a7a9ced990baaacb86
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-09-24 03:39:03 +0000

    Fixed linter issues

--- Microwave.swift
+++ Microwave.swift
@@ -10,27 +10,27 @@
 //  given a food choice and quantity.
 
 // Amount of time to reheat a single sub in seconds.
-let SUB_REHEAT_TIME = 60
+let subReheatTime = 60
 // Amount of time to reheat a single slice of pizza in seconds.
-let PIZZA_REHEAT_TIME = 45
+let pizzaReheatTime = 45
 // Amount of time to reheat a single bowl of soup in seconds.
 // 1 minute and 45 seconds.
-let SOUP_REHEAT_TIME = 105
+let soupReheatTime = 105
 // Array to hold all the food choices.
-let FOOD_CHOICES = [
+let foodChoices = [
     "SUB",
     "PIZZA",
     "SOUP"
 ]
 
 // Maximum quantity of food that the microwave can hold.
-let MAX_QUANTITY = 3
+let maxQuantity = 3
 
 // Constant to define the amount of seconds in a minute.
-let SECONDS_PER_MINUTE = 60
+let secondsPerMinute = 60
 
 // Display all food choices
-print("Food choices: \(FOOD_CHOICES)")
+print("Food choices: \(foodChoices)")
 // Prompt for food choice.
 print("Enter your food choice: ", terminator: "")
 // Get user's food choice as a string
@@ -38,38 +38,38 @@ let userFoodChoiceInput = readLine() ?? ""
 // Convert user input to uppercase to match array values.
 let userFoodChoice = userFoodChoiceInput.uppercased()
 // Check if the user input is a valid food choice.
-if FOOD_CHOICES.contains(userFoodChoice) {
+if foodChoices.contains(userFoodChoice) {
     // Prompt for quantity.
-    print("Enter the quantity (1-\(MAX_QUANTITY)): ", terminator: "")
+    print("Enter the quantity (1-\(maxQuantity)): ", terminator: "")
     // Get quantity as a string.
     if let quantityAsString = readLine() {
         // Convert quantity input to an integer.
         if let quantity = Int(quantityAsString) {
             // Check if the quantity is within the valid range.
-            if quantity >= 1 && quantity <= MAX_QUANTITY {
+            if quantity >= 1 && quantity <= maxQuantity {
                 // Initialize reheat time variable for a single item.
                 var baseReheatTime = 0
                 // Determine the reheat time based on food choice.
                 if userFoodChoice == "SUB" {
-                    baseReheatTime = SUB_REHEAT_TIME
+                    baseReheatTime = subReheatTime
                 } else if userFoodChoice == "PIZZA" {
-                    baseReheatTime = PIZZA_REHEAT_TIME
+                    baseReheatTime = pizzaReheatTime
                 } else if userFoodChoice == "SOUP" {
-                    baseReheatTime = SOUP_REHEAT_TIME
+                    baseReheatTime = soupReheatTime
                 }
                 // Apply the quantity formula on the base reheat time.
                 // To get the actual reheat time. [in seconds]
                 let actualReheatTime = Double(baseReheatTime) * Double(quantity + 1) * 0.5
                 // Convert pure seconds to a combination of minutes and seconds.
-                let minutes = Int(actualReheatTime / Double(SECONDS_PER_MINUTE))
-                let seconds = Int(actualReheatTime) % SECONDS_PER_MINUTE
+                let minutes = Int(actualReheatTime / Double(secondsPerMinute))
+                let seconds = Int(actualReheatTime) % secondsPerMinute
                 // Display the result. [IN BLUE]
                 print("\u{001B}[0;34mReheat time: \(minutes)", terminator: "")
                 print(" minute(s) and \(seconds) second(s).")
             } else {
                 // Error message for invalid quantity. [IN RED]
                 print("\u{001B}[0;31mERROR: INVALID QUANTITY", terminator: "")
-                print(" IT MUST BE BETWEEN 1 AND \(MAX_QUANTITY)")
+                print(", IT MUST BE BETWEEN 1 AND \(maxQuantity)")
             }
         } else {
             // Error message for non-integer quantity. [IN RED]
